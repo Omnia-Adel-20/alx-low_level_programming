a6612e93f7d4d0870b6940ad96287dcb64a94924
Omnia <omniaboss@gmail.com>
2023-07-14 12:05:39 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all the numbers in lowercase
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57 && i <= 102)
+	{
+		putchar(i);
+		i++,
+	}
+	putchar('\n');
+
+	return (0);
+}
