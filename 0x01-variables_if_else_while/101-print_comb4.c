235f0acdc737535c0ebbdde5419b0b12659bcad8
Omnia <omniaboss@gmail.com>
2023-07-14 14:03:08 -0700

    a

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible combination
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int d1 = 0;
+	int d2, d3;
+
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
+			d3 = 0;
+			while (d3 <= 9)
+			{
+				if (d1 != d2 && d1 < d2 && d2 != d3 && d2 < d3)
+				{
+					putchar(d1 + 48);
+					putchar(d2 + 48);
+					putchar(d3 + 48);
+
+					if (d1 + d2 + d3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				d3++;
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
