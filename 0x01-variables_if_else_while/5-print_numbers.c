4593d31e7b9b64b78e02381f0f733beb6acfb03c
Omnia <omniaboss@gmail.com>
2023-07-14 09:22:36 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet in lowercase
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	if (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf('\n');
+
+	return (0);
+}
