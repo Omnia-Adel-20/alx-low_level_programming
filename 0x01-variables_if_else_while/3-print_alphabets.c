42289f0ee300b9729ac288a9ac7bb80d796613cf
Omnia <omniaboss@gmail.com>
2023-07-14 08:34:26 -0700

    a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:  prints the alphabet in lowercase and uppercase
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+
+	return (0);
+}
