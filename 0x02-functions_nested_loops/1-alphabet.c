853a743b55d4ce901aa2239fc404178447a78069
Omnia <omniaboss@gmail.com>
2023-07-18 10:16:42 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Description: prints the alphabet, in lowercase
+ *
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
