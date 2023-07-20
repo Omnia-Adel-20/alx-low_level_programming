bf4ea318fd03ec8ce8cfe80e8f539165300e345e
Omnia <omniaboss@gmail.com>
2023-07-20 09:58:06 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_most_numbers - prints the numbers, from 0 to 9
- * Return: Always 0
+ * Return: void
 */
 
 void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c >= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
 			_putchar(c);
