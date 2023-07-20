6f95a9646c699e3da73094545bb6d06cc5aae7a4
Omnia <omniaboss@gmail.com>
2023-07-20 12:25:45 -0700

    a

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints the numbers from 1 to 100
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % == 15)
+			printf("FizzBuzz");
+		else if (n % == 3)
+			printf("Fizz");
+		else if (n % == 5)
+			printf("Buzz");
+		else
+			printf("%d", n);
+
+		if (i < 100)
+			pintf(" ");
+	}
+	pritnf("\n");
+	return (0);
+}
