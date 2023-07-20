7077ca79fb32e18e52f6da8b15f275f3ad5e197e
Omnia <omniaboss@gmail.com>
2023-07-20 13:13:25 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
 			printf("%d", i);
 
@@ -28,6 +28,5 @@ int main(void)
 		else
 			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
