db59e0f1dab2fce7d774714cb5853aeea6c04bf0
Omnia <omniaboss@gmail.com>
2023-07-24 11:49:54 -0700

    a

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - number in the string
+ *
+ * @s: infinite number of characters
+ *
+ * Return: 0 no number
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
