52377a2e12b413eadff6b5cadce341d91c88c1ad
Omnia <omniaboss@gmail.com>
2023-07-27 01:23:05 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,16 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i])
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for (j = 0; src[j], j++)
+	while (src[j] != '\0')
 	{
-		dest[i++] = src[j];
+		dest[i] = src[j]
+			i++;
+			j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
