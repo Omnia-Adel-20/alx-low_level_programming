695a00b1abedbbabd8c7c1280ed149a3db296f77
Omnia <omniaboss@gmail.com>
2023-08-02 11:16:58 -0700

    a

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function the value of x raised to the power of y
+ *
+ * @x: number
+ * @y: rasied number
+ * Return: y lower than 0 is -1
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (_pow_recursion(x, y));
+	}
+}
