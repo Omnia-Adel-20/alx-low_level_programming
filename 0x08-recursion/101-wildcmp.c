c3a490b393d7297c805c8168108fae1a08f735e8
Omnia <omniaboss@gmail.com>
2023-08-02 13:03:29 -0700

    a

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * move_past_star - past astrict
+ * @s2: second string
+ * Return: the pointer past
+*/
+
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * inception - make unique
+ * @s1: first sring
+ * @s2: second string
+ * Return: 0 or 1
+*/
+
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wilcmp - compare two string
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 or 1
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !*s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+	return (0);
+}
