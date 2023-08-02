58b1297235dff3281788f42f5924e2f2894b4382
Omnia <omniaboss@gmail.com>
2023-08-02 13:25:48 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -63,6 +63,8 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
 		if (*s1 == *s2)
 			ret += wildcmp(s1 + 1, s2 + 1);
 		ret += inception(s1, s2);
