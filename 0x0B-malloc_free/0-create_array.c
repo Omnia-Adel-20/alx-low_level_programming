a1e053e279c391d26bd68afc5cb4f2133602d8f5
Omnia <omniaboss@gmail.com>
2023-08-07 09:44:05 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - function that creates an array of chars
+ * @size: size of array
+ * @c: char
+ * Return: null or pointer
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+
+	if (size == 0 || n == 0)
+		return (0);
+
+	while (size--)
+		n[size] = c;
+
+	return (n);
+}
