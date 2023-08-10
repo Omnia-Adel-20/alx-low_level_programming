606b280d0c7ed8ef8f5991f5ab4f6496163b6329
Omnia <omniaboss@gmail.com>
2023-08-10 10:49:17 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,16 +30,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *m;
 
 	if (size == 0 || nmemb == 0)
-	{
 		return (NULL);
-	}
-	m = malloc(sizeof(int) * nmemb);
+	m = malloc(size * nmemb);
 
 	if (m == 0)
-	{
 		return (NULL);
-	}
-	_memset(m, 0, sizeof(int) * nmemb);
+	_memset(m, 0, size * nmemb);
 
 	return (m);
 }
