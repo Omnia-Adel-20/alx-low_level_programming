5ebf6eb8162b96571fd7ab88e1bb9e405c5f82d8
Omnia <omniaboss@gmail.com>
2023-08-14 09:37:26 -0700

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize
+ * @d: dog init
+ * @name: the dog's name
+ * @age: age
+ * @owner: owner name
+ * Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
