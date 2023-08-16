d63f55300458c52d330a7823ebf5b05793d91611
Omnia <omniaboss@gmail.com>
2023-08-16 11:24:53 -0700

    a

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,13 +18,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (i < 5)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
+		if (s && s[0] == (ops[i].op[0] && !s[1])
 				return (ops[i].f);
 		i++;
 	}
